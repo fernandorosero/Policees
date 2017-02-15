9a6acb3efbb3cb57c7a335839d900720a228ee05
xe51781 <xe51781@W19620DD.ad.bbva.com>
2017-02-15 18:55:40 +0100

    Curr

--- view/contacto.php
+++ view/contacto.php
@@ -10,49 +10,59 @@
 
         <!-- Latest compiled and minified JavaScript -->
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+        
+        
         <div class="col-md-1"></div>
         <div class="col-md-4" style="margin-bottom: 20px;">
-            
-            <?php
-            //include_once './ubicacion.php';
-            
-            ?>
+            <div class="container">
             <img src="img/map_ubicacion.jpg" style="margin-top: 50px;"/>
             <address style="margin-top: 28px;">
-                <strong>Empresa, S.A.</strong><br>
+                <strong class="text-primary">Empresa, S.A.</strong><br>
                 28044 Valdemoro, Madrid<br>
                 Madrid- Espa&ntilde;a<br>
                 <abbr title="Phone">Teléfono:</abbr> (34) 666-555-333
               </address>
 
               <address>
-                <strong>Correo de contacto</strong><br>
-                <a href="mailto:#">empresa.contactos@example.com</a>
+                  <strong>Correo de contacto</strong>
+                  <p style="
+                     margin-top: 10px;
+                     ">
+                <a href="mailto:#" 
+                   style="
+                    border: 1px solid #007fcf;
+                    padding: 0.5em 1em 0.5em 1em;
+                    margin: 0.3em 0 0 0;
+                    text-decoration: none;
+                    ">
+                    empresa.contactos@example.com
+                </a>
+                      </p>  
             </address>
+            </div>
         </div>
         <div class="col-md-1"></div>
     <div class="col-md-5">
-<form>
-     <h2 class="form-signin-heading text-center">Formulario de Contacto</h2>
-  <div class="form-group">
-    <label for="exampleInputEmail1">Nombre *</label>
-    <input type="text" class="form-control" id="exampleInputEmail1" placeholder="" required/>
-  </div>  
-  <div class="form-group">
-    <label for="exampleInputEmail1">Correo electr&oacute;nico *</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" placeholder="" required/>
-  </div>
-  <div class="form-group">
-      <label for="exampleInputPassword1">Tel&eacute;fono *</label>
-    <input type="text" class="form-control" id="exampleInputPassword1" placeholder="" required/>
-  </div>
-  <div class="form-group">
-    <label for="exampleInputFile">Mensaje *</label>
-    <textarea class="form-control" rows="5" required></textarea>
-  </div>
-  
-  <button type="submit" class="btn btn-info">Enviar</button>
-</form>
-        
-        </div>
-        <div class="col-md-1"></div>
+        <form>
+             <h2 class="form-signin-heading text-center">Formulario de Contacto</h2>
+          <div class="form-group">
+            <label for="exampleInputEmail1">Nombre *</label>
+            <input type="text" class="form-control" id="exampleInputEmail1" placeholder="" required/>
+          </div>  
+          <div class="form-group">
+            <label for="exampleInputEmail1">Correo electr&oacute;nico *</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" placeholder="" required/>
+          </div>
+          <div class="form-group">
+              <label for="exampleInputPassword1">Tel&eacute;fono *</label>
+            <input type="text" class="form-control" id="exampleInputPassword1" placeholder="" required/>
+          </div>
+          <div class="form-group">
+            <label for="exampleInputFile">Mensaje *</label>
+            <textarea class="form-control" rows="5" required></textarea>
+          </div>
+
+          <button type="submit" class="btn btn-info btn">Enviar</button>
+        </form>
+    </div>
+    <div class="col-md-1"></div>
