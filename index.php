fc8d738c24e7a7ace0a7fcc54b5aa2198ba4b71e
fernandorosero <tatofarp@hotmail.com>
2017-02-15 05:44:34 +0100

    Toshiba

--- index.php
+++ index.php
@@ -58,52 +58,94 @@ and open the template in the editor.
                  <div class="navbar-right">
                      <a class="navbar-brand" href="view/login.php">Acceso</a>
                 </div>
+                      <!-- Button trigger modal -->
+                <button type="button" class="btn btn-danger btn-lg navbar-right glyphicon glyphicon-log-in" data-toggle="modal" data-target="#myModal">
+                    Acceso
+                </button>
             </div>
         </nav>
         
         <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-  <!-- Indicators -->
-  <ol class="carousel-indicators">
-    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-  </ol>
+        <!-- Indicators -->
+        <ol class="carousel-indicators">
+          <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+          <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+          <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+        </ol>
 
-  <!-- Wrapper for slides -->
-  <div class="carousel-inner" role="listbox">
-    <div class="item active">
-        <img src="view/img/img6.jpg" alt="Primero">
-      <div class="carousel-caption">
-        banner uno
-      </div>
+        <!-- Wrapper for slides -->
+        <div class="carousel-inner" role="listbox">
+          <div class="item active">
+              <img src="view/img/img6.jpg" alt="Primero">
+            <div class="carousel-caption">
+              banner uno
+            </div>
+          </div>
+          <div class="item">
+              <img src="view/img/img4.jpg" alt="Segundo">
+            <div class="carousel-caption">
+              banner 2
+            </div>
+          </div>
+          <div class="item">
+              <img src="view/img/img5.jpg" alt="Segundo">
+            <div class="carousel-caption">
+              banner 3
+            </div>
+          </div>
+        </div>
+
+        <!-- Controls -->
+        <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+          <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+          <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
     </div>
-    <div class="item">
-        <img src="view/img/img4.jpg" alt="Segundo">
-      <div class="carousel-caption">
-        banner 2
+        
+        
+
+
+<!-- Modal -->
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title" id="myModalLabel">Acceso</h4>
       </div>
-    </div>
-    <div class="item">
-        <img src="view/img/img5.jpg" alt="Segundo">
-      <div class="carousel-caption">
-        banner 3
+      <div class="modal-body">
+        <!-- inicio formulario-->
+        <form>
+  <div class="form-group">
+    <label for="exampleInputEmail1">Email address</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+  </div>
+  <div class="checkbox">
+    <label>
+      <input type="checkbox"> Check me out
+    </label>
+  </div>
+  <button type="submit" class="btn btn-default">Submit</button>
+
+        
+        <!-- fin formulario -->
       </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Cancelar</button>
+        <button type="submit" class="btn btn-primary">Aceptar</button>
+      </div>
+        </form>
     </div>
   </div>
-
-  <!-- Controls -->
-  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
-    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
-    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
 </div>
-        
-        
-       
         
         
     </body>
