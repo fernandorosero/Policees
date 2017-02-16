274f69a4292d0ee9dc241e7bcee4b5608bb60d82
xe51781 <xe51781@W19620DD.ad.bbva.com>
2017-02-16 17:00:54 +0100

    Curr

---
+++ init_nivel_one.php
@@ -0,0 +1,34 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+?>
+
+ <!-- jQuery -->
+        <script src="view/js/jquery.js"></script>
+        
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+        <!-- Optional theme -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+        <!-- Latest compiled and minified JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+        
+        
+        
+        <!-- CSS -->
+        <link rel="stylesheet" href="view/css/custom.css"/>
+        <!--
+        <script type="text/javascript">
+            $(document).ready(function(){
+                $('.carousel').carousel({
+                    interval: 3000
+                })
+            });
+       </script>
+        -->
