8e1b9958bb1804aaf8ab7a3260393b30a1f8f0e2
xe51781 <xe51781@W19620DD.ad.bbva.com>
2017-02-16 17:04:53 +0100

    Curr

--- view/contacto.php
+++ view/contacto.php
@@ -1,17 +1,9 @@
 <?php
 ?>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<!-- jQuery -->
-<script src="js/jquery.js"></script>
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
-        <!-- Optional theme -->
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-
-        <!-- Latest compiled and minified JavaScript -->
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-        
-        
+  <?php
+    include_once './init_nivel_two.php';
+  ?>
         <div class="col-md-1"></div>
         <div class="col-md-4" style="margin-bottom: 20px;">
             <div class="container">
