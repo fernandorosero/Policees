4f07170fc77ab6985dabeb01b23803dacefad156
xe51781 <xe51781@W19620DD.ad.bbva.com>
2017-02-16 19:26:32 +0100

    Curr

--- view/alarmas.php
+++ view/alarmas.php
@@ -22,6 +22,7 @@
         
     </div>
     -->
+   
     <div class="navbar-header">
       <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
         <span class="sr-only">Toggle navigation</span>
@@ -29,12 +30,14 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="#">Brand</a>
+      <!--<a class="navbar-brand" href="#">Brand</a>-->
     </div>
 
     <!-- Collect the nav links, forms, and other content for toggling -->
+   
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-      <ul class="nav navbar-nav">
+      <!--
+        <ul class="nav navbar-nav">
         <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
         <li><a href="#">Link</a></li>
         <li class="dropdown">
@@ -50,23 +53,34 @@
           </ul>
         </li>
       </ul>
+      -->
+      <!--
       <form class="navbar-form navbar-left">
         <div class="form-group">
           <input type="text" class="form-control" placeholder="Search">
         </div>
         <button type="submit" class="btn btn-default">Submit</button>
       </form>
+      -->
       <ul class="nav navbar-nav navbar-right">
-        <li><a href="#">Link</a></li>
+        <li><a href="#">Inicio</a></li>
         <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Productos <span class="caret"></span></a>
           <ul class="dropdown-menu">
-            <li><a href="#">Action</a></li>
-            <li><a href="#">Another action</a></li>
-            <li><a href="#">Something else here</a></li>
+            <li><a href="#">Alarmas</a></li>
             <li role="separator" class="divider"></li>
-            <li><a href="#">Separated link</a></li>
+            <li><a href="#">C&aacute;maras</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">Control de Horarios</a></li>
           </ul>
+        </li>
+        <li><a href="#">Contacto</a></li>
+        <li><button class="btn btn-left">Acceso</button></li>
+        <li>
+            
+           <form class="navbar-form navbar-left">
+            <button type="submit" class="btn btn-default">Submit</button>
+           </form> 
         </li>
       </ul>
     </div><!-- /.navbar-collapse -->
