4bd158d5f27321621659adb19db335e9d338347c
fernandorosero <tatofarp@hotmail.com>
2017-02-22 23:53:09 +0100

    Toshiba

--- dashboard.php
+++ dashboard.php
@@ -2,3 +2,39 @@
    include_once './init_nivel_one.php';
    include_once './nav-bar_nivel1.php';
 ?>
+
+<h2>Dashboard</h2>
+
+<div class="container">
+    
+    <div class="row">
+        <div class="col-md-3 vistaDashboard">
+            Usuarios
+        </div>
+        <div class="col-md-3 vistaDashboard">
+            Contactos
+        </div>
+        <div class="col-md-3 vistaDashboard">
+            Productos
+        </div>
+        <div class="col-md-3 vistaDashboard">
+            Clientes
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-3 vistaDashboard">
+            Documentos
+        </div>
+        <div class="col-md-3 vistaDashboard">
+            Videos
+        </div>
+        <div class="col-md-3 vistaDashboard">
+            Productos
+        </div>
+        <div class="col-md-3 vistaDashboard">
+            Clientes
+        </div>
+    </div>
+    
+    
+</div>
