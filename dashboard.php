56b1f1acaf9cda8635a9d6a2ee20427ab2e75204
fernandorosero <tatofarp@hotmail.com>
2017-02-23 00:46:42 +0100

    Toshiba

--- dashboard.php
+++ dashboard.php
@@ -8,32 +8,74 @@
 <div class="container">
     
     <div class="row">
-        <div class="col-md-3 vistaDashboard">
-            Usuarios
+        <div class="col-sm-3 col-md-3 vistaDashboard">
+            <div class="thumbnail">
+                <div class="caption text-center">
+                    <p>Usuarios</p>
+                    <img src="view/img/camara-150x150.jpg"/>
+                </div>
+            </div>
         </div>
-        <div class="col-md-3 vistaDashboard">
-            Contactos
+        <div class="col-sm-3 col-md-3 vistaDashboard">
+            <div class="thumbnail">
+                <div class="caption text-center">
+                    <p>Contactos</p>
+                    <img src="view/img/camara-150x150.jpg"/>
+                </div>
+            </div>
         </div>
-        <div class="col-md-3 vistaDashboard">
-            Productos
+        <div class="col-sm-3 col-md-3 vistaDashboard">
+            <div class="thumbnail">
+                <div class="caption text-center">
+                    <p>Productos</p>
+                    <img src="view/img/camara-150x150.jpg"/>
+                </div>
+            </div>
         </div>
-        <div class="col-md-3 vistaDashboard">
-            Clientes
+        <div class="col-sm-3 col-md-3 vistaDashboard">
+            <div class="thumbnail">
+                <div class="caption text-center">
+                    <p>Clientes</p>
+                    <img src="view/img/camara-150x150.jpg"/>
+                </div>
+            </div>
         </div>
+        
     </div>
     <div class="row">
-        <div class="col-md-3 vistaDashboard">
-            Documentos
+        <div class="col-sm-3 col-md-3 vistaDashboard">
+            <div class="thumbnail">
+                <div class="caption text-center">
+                    <p>Documentos</p>
+                    <img src="view/img/camara-150x150.jpg"/>
+                </div>
+            </div>
         </div>
-        <div class="col-md-3 vistaDashboard">
-            Videos
+        <div class="col-sm-3 col-md-3 vistaDashboard">
+            <div class="thumbnail">
+                <div class="caption text-center">
+                    <p>Videos</p>
+                    <img src="view/img/camara-150x150.jpg"/>
+                </div>
+            </div>
         </div>
-        <div class="col-md-3 vistaDashboard">
-            Productos
+        <div class="col-sm-3 col-md-3 vistaDashboard">
+            <div class="thumbnail">
+                <div class="caption text-center">
+                    <p>Productos</p>
+                    <img src="view/img/camara-150x150.jpg"/>
+                </div>
+            </div>
         </div>
-        <div class="col-md-3 vistaDashboard">
-            Clientes
+        <div class="col-sm-3 col-md-3 vistaDashboard">
+            <div class="thumbnail">
+                <div class="caption text-center">
+                    <p>Clientes</p>
+                    <img src="view/img/camara-150x150.jpg"/>
+                </div>
+            </div>
         </div>
+        
     </div>
     
     
