eddc024d33143787d3004422c53113a0d860435f
xe51781 <xe51781@W19620DD.ad.bbva.com>
2017-02-27 15:44:38 +0100

    Curr

--- contacto.php
+++ contacto.php
@@ -5,6 +5,33 @@
 <div class="contactoBloque">
 <div class="container">
         <div class="col-md-1"></div>
+        <div class="col-md-5">
+        <form>
+             <h2 class="form-signin-heading text-center">Formulario de Contacto</h2>
+          <div class="form-group">
+            <label for="exampleInputEmail1">Nombre *</label>
+            <input type="text" class="form-control" id="exampleInputEmail1" placeholder="" required/>
+          </div>  
+          <div class="form-group">
+            <label for="exampleInputEmail1">Correo electr&oacute;nico *</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" placeholder="" required/>
+          </div>
+          <div class="form-group">
+              <label for="exampleInputPassword1">Tel&eacute;fono *</label>
+            <input type="text" class="form-control" id="exampleInputPassword1" placeholder="" required/>
+          </div>
+          <div class="form-group">
+            <label for="exampleInputFile">Mensaje *</label>
+            <textarea class="form-control" rows="5" required></textarea>
+          </div>
+             <a href="../index.php" class="btn btn-danger">Cancelar</a>   
+          <button type="submit" class="btn btn-info btn">Enviar</button>
+        </form>
+    </div>
+        
+        <div class="col-md-1"></div>
+    
+    
         <div class="col-md-4" style="margin-bottom: 20px;">
             <div class="container">
                 <img src="view/img/map_ubicacion.jpg" style="margin-top: 50px;"/>
@@ -34,30 +61,6 @@
             </div>
         </div>
         <div class="col-md-1"></div>
-    <div class="col-md-5">
-        <form>
-             <h2 class="form-signin-heading text-center">Formulario de Contacto</h2>
-          <div class="form-group">
-            <label for="exampleInputEmail1">Nombre *</label>
-            <input type="text" class="form-control" id="exampleInputEmail1" placeholder="" required/>
-          </div>  
-          <div class="form-group">
-            <label for="exampleInputEmail1">Correo electr&oacute;nico *</label>
-            <input type="email" class="form-control" id="exampleInputEmail1" placeholder="" required/>
-          </div>
-          <div class="form-group">
-              <label for="exampleInputPassword1">Tel&eacute;fono *</label>
-            <input type="text" class="form-control" id="exampleInputPassword1" placeholder="" required/>
-          </div>
-          <div class="form-group">
-            <label for="exampleInputFile">Mensaje *</label>
-            <textarea class="form-control" rows="5" required></textarea>
-          </div>
-             <a href="../index.php" class="btn btn-danger">Cancelar</a>   
-          <button type="submit" class="btn btn-info btn">Enviar</button>
-        </form>
-    </div>
-    <div class="col-md-1"></div>
     
     </div>
     
--- index.php
+++ index.php
@@ -8,13 +8,7 @@
         <title>__________________________<!--Policees Seguridad - Demo nada---></title>
         <!-- Latest compiled and minified CSS -->
        <script>
-$(function () {
-  var nua = navigator.userAgent
-  var isAndroid = (nua.indexOf('Mozilla/5.0') > -1 && nua.indexOf('Android ') > -1 && nua.indexOf('AppleWebKit') > -1 && nua.indexOf('Chrome') === -1)
-  if (isAndroid) {
-    $('select.form-control').removeClass('form-control').css('width', '100%')
-  }
-})
+
 </script>
     </head>
     <body>
@@ -23,53 +17,10 @@ $(function () {
             print("<!--<h1 class='titulo_demo'>HOME POLICEES-Demo- nada</h1>-->");
             include_once './nav-bar_nivel1.php';
         ?>
-        <!-- inicio carousel -->
-        <div class="carouselPolicees">
-            
         
-        <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-        <!-- Indicators -->
-        <ol class="carousel-indicators">
-          <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-          <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-          <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-        </ol>
-
-        <!-- Wrapper for slides -->
-        <div class="carousel-inner" role="listbox">
-          <div class="item active">
-              <img src="view/img/img7.jpg" alt="Primero">
-            <div class="carousel-caption">
-              banner uno
-            </div>
-          </div>
-          <div class="item">
-              <img src="view/img/img4.jpg" alt="Segundo">
-            <div class="carousel-caption">
-              banner 2
-            </div>
-          </div>
-          <div class="item">
-              <img src="view/img/img5.jpg" alt="Segundo">
-            <div class="carousel-caption">
-              banner 3
-            </div>
-          </div>
-        </div>
-
-        <!-- Controls -->
-        <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
-          <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-          <span class="sr-only">Previous</span>
-        </a>
-        <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
-          <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-          <span class="sr-only">Next</span>
-        </a>
-    </div>
-            </div>
-        <!-- Fin carousel -->
-        
+        <?php
+        include_once './carousel.php';
+        ?>
 
 
         <!-- Modal -->
