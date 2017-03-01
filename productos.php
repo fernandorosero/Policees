f46eb6b8c73c3b12cc2edd853aa85d645c2582aa
fernandorosero <tatofarp@hotmail.com>
2017-03-01 01:20:57 +0100

    Lenovo-Casa

--- productos.php
+++ productos.php
@@ -1,43 +1,43 @@
 <div id="productos">
-    <h2>Productos</h2>
+    <h2 class="text-center">Productos</h2>
     <div class="row" style="margin-top: 40px;">
         <div class="col-sm-6 col-md-4">
-            <div class="thumbnail">
-                <h3>C&aacute;maras de seguridad</h3>
+            <a href="view/camaras.php" class="thumbnail noLineaEnlace">
+                <h3 class="text-center"><b>C&aacute;maras de seguridad</b></h3>
                 <img src="view/img/camara-150x150.jpg" alt="camara">
                 <div class="caption">
-                    <p>Especialistas en instalación y manteniemiento de cámaras de seguridad</p>
-                    <p>
+                    <p class="text-justify">Especialistas en instalación y manteniemiento de cámaras de seguridad</p>
+                    <!--<p>
                         <a href="view/camaras.php" class="btn btn-lg btn-primary btn-block" role="button">Ir a Cámaras</a>
-                    </p>
+                    </p> -->
                 </div>
-            </div>
+            </a>
         </div>
 
         <div class="col-sm-6 col-md-4">
-            <div class="thumbnail">
-                <h3>Alarmas</h3>
+            <a href="view/alarmas.php" class="thumbnail noLineaEnlace">
+                <h3 class="text-center"><b>Alarmas</b></h3>
                 <img src="view/img/detalle1-150x150.jpg" alt="camara">
                 <div class="caption">
-                    <p>Especialistas en instalación y manteniemiento de alarmas en todos los modelos</p>
-                    <p>
+                    <p class="text-justify">Especialistas en instalación y manteniemiento de alarmas en todos los modelos</p>
+                    <!--<p>
                         <a href="view/camaras.php" class="btn  btn-lg btn-primary btn-block" role="button">Ir a Alarmas</a>
-                    </p>
+                    </p> -->
                 </div>
-            </div>
+            </a>
         </div>
 
         <div class="col-sm-6 col-md-4">
-            <div class="thumbnail">
-                <h3>Control de accesos</h3>
+            <a href="view/control-horarios.php" class="thumbnail noLineaEnlace">
+                <h3 class="text-center">Control de accesos</h3>
                 <img src="view/img/control_acceso-300x300.jpg" alt="camara" style="height: 150px;">
             <div class="caption">
-              <p>Especialistas en instalación y manteniemiento de Control de accesos, para empresas y particulares</p>
-              <p>
+              <p class="text-justify">Especialistas en instalación y manteniemiento de Control de accesos, para empresas y particulares</p>
+           <!--   <p>
                   <a href="view/camaras.php" class="btn  btn-lg btn-primary btn-block" role="button">Ir a Control Horarios</a>
-              </p>
-            </div>
-          </div>
+              </p> -->
+             </div>
+          </a>
         </div>    
-  </div>
+    </div>
 </div>
