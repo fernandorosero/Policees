65f3febc2d76d1bfeb66874762dd9b522153873d
fernandorosero <tatofarp@hotmail.com>
2017-03-02 02:12:52 +0100

    Toshiba

---
+++ imagenFondo.php
@@ -0,0 +1,8 @@
+<!--
+<img src="view/img/PoliceesCoorporative.jpg" class="image-bg fullscreen overlay bg-dark vid-bg" alt="PoliceesCoorporative"/>
+-->
+
+<div id="imagenFondo">
+<img id="background" class="img" src="view/img/PoliceesCoorporative.jpg" alt="" title=""/>
+</div>
+<div></div>
--- nav-bar_nivel1.php
+++ nav-bar_nivel1.php
@@ -32,7 +32,7 @@
 
     <!-- Collect the nav links, forms, and other content for toggling -->
    
-    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1" style="margin-bottom: 5px;">
       <!--
         <ul class="nav navbar-nav">
         <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
