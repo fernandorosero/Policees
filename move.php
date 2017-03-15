4a3c50b9ad3969949a06ed9db4ee4177c0add5c1
xe51781 <xe51781@W19620DD.ad.bbva.com>
2017-03-15 18:56:38 +0100

    Curr

---
+++ move.php
@@ -0,0 +1,15 @@
+<?php
+    include_once './init_nivel_one.php';
+?>
+
+<div id="contenedorIMG">
+    <img class="lrobo" src="view/img/roboleft.png"/>
+    <img class="rrobot"  src="view/img/roboright.png"/>
+    <img class="robot"  src="view/img/robo.png"/>
+    <img class="nubeone"  src="view/img/nube1.png"/>
+    <img class="nubetwo"  src="view/img/nube2.png"/>
+    <img class="nubetree"  src="view/img/nube3.png"/>
+    
+    
+</div>
+
